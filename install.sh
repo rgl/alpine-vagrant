ce769593274b76419a7c718189587028e64097b3
Rui Lopes <rgl@ruilopes.com>
2022-12-18 13:36:18 +0000

    upgrade to alpine 3.17.0

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ iface eth0 inet dhcp
 DNSOPTS=""
 TIMEZONEOPTS="-z UTC"
 PROXYOPTS="none"
-APKREPOSOPTS="http://mirrors.dotsrc.org/alpine/v3.16/main"
+APKREPOSOPTS="http://mirrors.dotsrc.org/alpine/v3.17/main"
 USEROPTS="-a vagrant"
 SSHDOPTS="-c openssh"
 NTPOPTS="-c chrony"
